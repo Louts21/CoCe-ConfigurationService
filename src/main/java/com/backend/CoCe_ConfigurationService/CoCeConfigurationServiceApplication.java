7d5be84edf9eed143999762177dfcc6dda6772ab
Louts21 <72473132+Louts21@users.noreply.github.com>
2026-03-18 15:01:47 +0100

    initialized project

---
+++ src/main/java/com/backend/CoCe_ConfigurationService/CoCeConfigurationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.backend.CoCe_ConfigurationService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoCeConfigurationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoCeConfigurationServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/backend/CoCe_ConfigurationService/CoCeConfigurationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.CoCe_ConfigurationService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoCeConfigurationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
